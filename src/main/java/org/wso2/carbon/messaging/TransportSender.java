62bb7288d195b022f53430e35ab0731ff08910fe
Senduran <bsenduran@gmail.com>
2015-11-24 16:54:58 +0530

    adding TransportSender interface

---
+++ src/main/java/org/wso2/carbon/messaging/TransportSender.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.messaging;
+
+import org.wso2.carbon.messaging.CarbonCallback;
+import org.wso2.carbon.messaging.CarbonMessage;
+
+/**
+ * Interface for Message Sender to the BE.
+ */
+public interface TransportSender {
+    /**
+     * Should include the logic for handover messages to BE.
+     * @param msg Mediated Request.
+     * @param callback Carbon callback created by engine.
+     * @return void
+     * @throws EngineException Gateway exception to singal any failures to upper layers.
+     */
+    public boolean send(CarbonMessage msg, CarbonCallback callback) throws EngineException;
+
+}
---
+++ src/main/java/org/wso2/carbon/messaging/internal/ContextHolder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.messaging.internal;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.wso2.carbon.messaging.CarbonMessageProcessor;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class ContextHolder {
+
+    public ContextHolder() {
+    }
+
+    private static ContextHolder instance = new ContextHolder();
+    private BundleContext bundleContext;
+    private Map<String, CarbonMessageProcessor> engines = new HashMap<>();
+
+    public static ContextHolder getInstance() {
+        return instance;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+
+    public void addEngine(CarbonMessageProcessor carbonMessageProcessor) {
+        engines.put("somename", carbonMessageProcessor);
+    }
+
+    public void removeEngine(CarbonMessageProcessor carbonMessageProcessor) {
+        engines.remove("somename");
+    }
+}
