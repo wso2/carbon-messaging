a42869c38f7b633e87b040f1acba99b5f74d1917
shafreen <shafreen@wso2.com>
2016-02-10 09:04:58 +0530

    Added code to allow extension points for the GW

---
+++ components/src/main/java/org/wso2/carbon/messaging/Interceptor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.messaging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+/**
+ * The class that is responsible for engaging all the interceptors.
+ */
+public class Interceptor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Interceptor.class);
+    private ArrayList<MessagingHandler> handlers = new ArrayList<>();
+
+    public boolean engage(CarbonMessage carbonMessage, EngagedLocation engagedLocation) {
+        try {
+            for (MessagingHandler mHandler : handlers) {
+                mHandler.invoke(carbonMessage, engagedLocation);
+            }
+        } catch (Exception e) {
+            LOG.error("Error while executing handler", e);
+        }
+        return true;
+    }
+}
