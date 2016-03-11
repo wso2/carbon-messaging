529136d45e29b38ca30d1f68a9558a8508450c89
Senduran <bsenduran@gmail.com>
2016-03-11 15:15:06 +0530

    adding new methods to Messaging handler

---
+++ components/src/main/java/org/wso2/carbon/messaging/handler/MessagingHandler.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.messaging.handler;
+
+import org.wso2.carbon.messaging.CarbonCallback;
+import org.wso2.carbon.messaging.CarbonMessage;
+
+/**
+ *                 +------------+------------------+------------+
+ *            5    |            |  6           7   |            |   8
+ *            +--------------------->          +--------------------->
+ *                 |            |                  |            |
+ * 1          2    | Transport  |      Message     | Transport  |  3           4
+ * +-----------+   | Listener   |     Processor    |  Sender    |  +-----------+
+ *                 |            |                  |            |
+ *             12  |            |  11           10 |            |    9
+ *            <---------------------+          <---------------------+
+ *                 |            |                  |            |
+ *                 +------------+------------------+------------+
+ *
+ * 1.  source connection initiated
+ * 2.  source connection terminated
+ * 3.  target connection initiated
+ * 4.  target connection terminated
+ * 5.  source request receiving (start reading the message from client)
+ * 6.  source request sending (message reading completed, i.e sending to Message processor)
+ * 7.  target request receiving (processed message, receiving from Message processor to send to back-end)
+ * 8.  target request sending (sending message to back-end)
+ * 9.  target response receiving (start reading the response from back-end)
+ * 10. target response sending (response reading completed, i.e sending to Message processor)
+ * 11. source response receiving (processed response receiving from Message processor to send to client)
+ * 12. source response sending (sending response to client)
+ *
+ */
+
+public interface MessagingHandler {
+
+    //todo description + diagram update + impl
+    boolean validateRequestContinuation(CarbonMessage carbonMessage, CarbonCallback callback);
+
+    /**
+     * Invoked when source connection is initiated
+     * @param metadata unique string key to identify the connection
+     */
+    void invokeAtSourceConnectionInitiation(String metadata);
+
+    /**
+     * Invoked when source connection is terminated
+     * @param metadata unique string key to identify the connection
+     */
+    void invokeAtSourceConnectionTermination(String metadata);
+
+    /**
+     * Invoked when target connection is initiated
+     * @param metadata unique string key to identify the connection
+     */
+    void invokeAtTargetConnectionInitiation(String metadata);
+
+    /**
+     * Invoked when target connection is terminated
+     * @param metadata unique string key to identify the connection
+     */
+    void invokeAtTargetConnectionTermination(String metadata);
+
+    /**
+     * Invoked when source request is started receiving at source handler
+     * @param carbonMessage newly created carbon message.
+     */
+    void invokeAtSourceRequestReceiving(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when source request is started sending to the message processor
+     * @param carbonMessage client request (i.e headers, property and message body)
+     */
+    void invokeAtSourceRequestSending(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when the request is received again to the transport level after being processed at message processor
+     * @param carbonMessage processed (or mediated) request (i.e headers, properties and message body)
+     */
+    void invokeAtTargetRequestReceiving(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when the request is started sending to the backend.
+     * @param carbonMessage sent request (i.e the message is already had started to send to backend)
+     *                      So no message body will be available. Even though the headers and properties are available,
+     *                      manipulating them won't change the request send to the back end (because the headers are
+     *                      already been send to the backend)
+     */
+    void invokeAtTargetRequestSending(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when target response is started receiving at target handler
+     * @param carbonMessage newly created carbon message.
+     */
+    void invokeAtTargetResponseReceiving(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when target response is started sending to the message processor
+     * @param carbonMessage target response (i.e headers, property and message body)
+     */
+    void invokeAtTargetResponseSending(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when the response is received again to the transport level after being processed at message processor
+     * @param carbonMessage processed (or mediated) response (i.e headers, properties and message body)
+     */
+    void invokeAtSourceResponseReceiving(CarbonMessage carbonMessage);
+
+    /**
+     * Invoked when the response is started sending to the client.
+     * @param carbonMessage sent response (i.e with empty message body.
+     *                      similar carbon message to {@link #invokeAtTargetRequestSending(CarbonMessage)}
+     */
+    void invokeAtSourceResponseSending(CarbonMessage carbonMessage);
+
+    /**
+     * Gives handler name
+     * @return handler name
+     */
+    String handlerName();
+}
