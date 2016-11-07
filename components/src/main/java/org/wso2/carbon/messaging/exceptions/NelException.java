1706ced8372cd9afa7f2d1876381799d1640c30a
Hasitha Aravinda <mail.hasitha27@gmail.com>
2016-11-07 14:19:26 +0530

    introducing MessagingException.

--- components/src/main/java/org/wso2/carbon/messaging/exceptions/NelException.java
+++ components/src/main/java/org/wso2/carbon/messaging/exceptions/NelException.java
@@ -2,7 +2,9 @@ package org.wso2.carbon.messaging.exceptions;
 
 /**
  * This is to represent NEL Specific exceptions.
+ * @deprecated This class will be replaced by {@link MessagingException }
  */
+@Deprecated
 public class NelException extends Exception {
     // This class here only for type casting.
 }
