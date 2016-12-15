fd7a9467c92ca3edf1c003af4e820b944963c481
irunika <irunikail@gmail.com>
2016-12-15 10:26:29 +0530

    Added minor changes to comments

--- components/src/main/java/org/wso2/carbon/messaging/websocket/CloseWebSocketCarbonMessage.java
+++ components/src/main/java/org/wso2/carbon/messaging/websocket/CloseWebSocketCarbonMessage.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) ${date}, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *   Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *   WSO2 Inc. licenses this file to you under the Apache License,
  *   Version 2.0 (the "License"); you may not use this file except
@@ -46,7 +46,7 @@ public class CloseWebSocketCarbonMessage extends WebSocketCarbonMessage {
     }
 
     /**
-     * @return Status code of the reason to close
+     * @return Status code of the reason to close the connection
      */
     public int getStatusCode() {
         return statusCode;
